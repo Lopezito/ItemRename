8dd9321d22133e9bd61eaa160ad9ca36ec48162d
Agustin Lopez <postgamer@hotmail.com.ar>
2013-01-27 16:00:46 -0200

    ItemRename

--- src/com/gmail/lopezitospriter/ItemName/Comando.java
+++ src/com/gmail/lopezitospriter/ItemName/Comando.java
@@ -12,10 +12,44 @@ import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.inventory.meta.ItemMeta;
 
 public class Comando implements CommandExecutor {
-    
+	main plugin;
+	 
+	public Comando(main instance) {
+	plugin = instance;
+	}
+	
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
-		if(commandLabel.equalsIgnoreCase("renamee")) {
+		//Rename an item without enchantment
+		if(commandLabel.equalsIgnoreCase("rename")) {
+			//Rename
+		    if (args.length > 3) {
+			      Player p = (Player)sender;
+						  String arg1 = args[0];
+						  String arg4 = args[2].toUpperCase();
+					      PlayerInventory inventory = p.getInventory();
+					      ItemStack itemstack = new ItemStack(Material.matchMaterial(arg1), Integer.parseInt(args[1])); 
+					      ItemMeta test = (ItemMeta) itemstack.getItemMeta();
+					      StringBuilder string = new StringBuilder();
+					      for (int s = 3; s < args.length; s++) {
+					      if (s > 3) string.append(" ");
+					      string.append(args[s]);
+					      }
+					      String newnameitem = string.toString();
+					      test.setDisplayName(ChatColor.valueOf(arg4) + newnameitem);
+					      itemstack.setItemMeta(test);
+					      inventory.addItem(itemstack);
+					      String itemrenamed = Material.matchMaterial(arg1).name();
+				    	  p.sendMessage(ChatColor.GREEN + plugin.getConfig().getString("msg1") + " " + itemrenamed.toLowerCase() + ChatColor.WHITE + " " + plugin.getConfig().getString("msg2") + " " + ChatColor.valueOf(arg1) + newnameitem);
+				    	  p.sendMessage(ChatColor.GREEN + plugin.getConfig().getString("msg3"));
+				return true;
+		    } else {
+			    Player p = (Player)sender;
+		    	p.sendMessage(plugin.getCommand("rename").getUsage());
+		    }
+		  }
+		//Rename an item with enchantment
+		else if(commandLabel.equalsIgnoreCase("renamee")) {
 		    if (args.length > 3) {
 				      Player p = (Player)sender;
 						  String arg1 = args[0];
@@ -34,33 +68,76 @@ public class Comando implements CommandExecutor {
 					      itemstack.setItemMeta(test);
 					      itemstack.addEnchantment(Enchantment.getByName(arg5), 1);
 					      inventory.addItem(itemstack);
-				    	  p.sendMessage(ChatColor.GREEN + "You rename: " + arg1 + " to " + ChatColor.valueOf(arg4) + newnameitem);
-				    	  p.sendMessage(ChatColor.GREEN + "You see inventory!");
+					      String itemrenamed = Material.matchMaterial(arg1).name();
+				    	  p.sendMessage(ChatColor.GREEN + plugin.getConfig().getString("msg1") + " " + itemrenamed.toLowerCase() + ChatColor.WHITE + " " + plugin.getConfig().getString("msg2") + " " + ChatColor.valueOf(arg1) + newnameitem);
+				    	  p.sendMessage(ChatColor.GREEN + plugin.getConfig().getString("msg3"));
 				return true;
+		    } else {
+			    Player p = (Player)sender;
+		    	p.sendMessage(plugin.getCommand("renamee").getUsage());
 		    }
 		  }
-		if(commandLabel.equalsIgnoreCase("rename")) {
-		    if (args.length > 3) {
-				      Player p = (Player)sender;
-						  String arg1 = args[0];
-						  String arg4 = args[2].toUpperCase();
-					      PlayerInventory inventory = p.getInventory();
-					      ItemStack itemstack = new ItemStack(Material.matchMaterial(arg1), Integer.parseInt(args[1])); 
-					      ItemMeta test = (ItemMeta) itemstack.getItemMeta();
-					      StringBuilder string = new StringBuilder();
-					      for (int s = 3; s < args.length; s++) {
-					      if (s > 3) string.append(" ");
-					      string.append(args[s]);
-					      }
-					      String newnameitem = string.toString();
-					      test.setDisplayName(ChatColor.valueOf(arg4) + newnameitem);
-					      itemstack.setItemMeta(test);
-					      inventory.addItem(itemstack);
-				    	  p.sendMessage(ChatColor.GREEN + "You rename: " + arg1 + " to " + ChatColor.valueOf(arg4) + newnameitem);
-				    	  p.sendMessage(ChatColor.GREEN + "You see inventory!");
+		//Rename the item on your hand without enchantment
+		else if(commandLabel.equalsIgnoreCase("renamehand")) {
+			if (args.length > 0) {
+				Player p = (Player)sender;
+				String arg1 = args[0].toUpperCase();
+	            ItemStack itemstack = p.getInventory().getItemInHand();
+			      String itemrenamed = itemstack.getItemMeta().getDisplayName();
+			      ItemMeta test = (ItemMeta) itemstack.getItemMeta();
+			      StringBuilder string = new StringBuilder();
+			      for (int s = 1; s < args.length; s++) {
+			      if (s > 1) string.append(" ");
+			      string.append(args[s]);
+			      }
+			      String newnameitem = string.toString();
+			      test.setDisplayName(ChatColor.valueOf(arg1) + newnameitem);
+			      itemstack.setItemMeta(test);
+			      p.getInventory().setItemInHand(itemstack);
+		    	  p.sendMessage(ChatColor.GREEN + plugin.getConfig().getString("msg1") + " " + itemrenamed.toLowerCase() + ChatColor.WHITE +  " " + plugin.getConfig().getString("msg2") + " " + ChatColor.valueOf(arg1) + newnameitem);
+		    	  p.sendMessage(ChatColor.GREEN + plugin.getConfig().getString("msg3"));
 				return true;
+			} else {
+			    Player p = (Player)sender;
+		    	p.sendMessage(plugin.getCommand("renamehand").getUsage());
 		    }
 		  }
+		//Rename the item on your hand with enchantment
+		else if(commandLabel.equalsIgnoreCase("renameehand")) {
+		    if (args.length > 1) {
+				Player p = (Player)sender;
+				String arg1 = args[0].toUpperCase();
+				String arg2 = args[1].toUpperCase();
+	            ItemStack itemstack = p.getInventory().getItemInHand();
+			      String itemrenamed = itemstack.getItemMeta().getDisplayName();
+			      ItemMeta test = (ItemMeta) itemstack.getItemMeta();
+			      StringBuilder string = new StringBuilder();
+			      for (int s = 2; s < args.length; s++) {
+			      if (s > 2) string.append(" ");
+			      string.append(args[s]);
+			      }
+			      String newnameitem = string.toString();
+			      test.setDisplayName(ChatColor.valueOf(arg1) + newnameitem);
+			      itemstack.setItemMeta(test);
+			      itemstack.addEnchantment(Enchantment.getByName(arg2), 1);
+			      p.getInventory().setItemInHand(itemstack);
+		    	  p.sendMessage(ChatColor.GREEN + plugin.getConfig().getString("msg1") + " " + itemrenamed.toLowerCase() + ChatColor.WHITE +  " " + plugin.getConfig().getString("msg2") + " " + ChatColor.valueOf(arg1) + newnameitem);
+		    	  p.sendMessage(ChatColor.GREEN + plugin.getConfig().getString("msg3"));
+				return true;
+		    } else {
+			    Player p = (Player)sender;
+		    	p.sendMessage(plugin.getCommand("renameehand").getUsage());
+		    }
+		  }
+		else if(commandLabel.equalsIgnoreCase("itemrename")) {
+				Player p = (Player)sender;
+				p.sendMessage(ChatColor.RED + "- ItemRename/Help -");
+				p.sendMessage(ChatColor.GOLD + "/rename" + ChatColor.WHITE + ": This command is used to change the name of the item without adding enchants");
+				p.sendMessage(ChatColor.GOLD + "/renamee" + ChatColor.WHITE + ": This command is used to change the name of the item with adding enchants");
+				p.sendMessage(ChatColor.GOLD + "/renamehand" + ChatColor.WHITE + ": This command is to change the name of the item you have on hand without adding enchants");
+				p.sendMessage(ChatColor.GOLD + "/renameehand" + ChatColor.WHITE + ": This command is to change the name of the item you have on hand with adding enchants");
+				p.sendMessage(ChatColor.RED + "- Created by Lopezito -");
+		}
 		return true;
 	}
 
--- src/com/gmail/lopezitospriter/ItemName/main.java
+++ src/com/gmail/lopezitospriter/ItemName/main.java
@@ -1,19 +1,54 @@
 package com.gmail.lopezitospriter.ItemName;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.logging.Logger;
 
+import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.mcstats.BukkitMetrics;
 
 public class main extends JavaPlugin {
 	private Logger log = Logger.getLogger("Minecraft");
+
+    public void load() {
+    	if (!getDataFolder().exists())
+			getDataFolder().mkdir();
+    	try{
+			getConfig().load(new File(getDataFolder(), "config.yml"));
+    	}catch(FileNotFoundException e){
+			log.info("[" + getDescription().getName() + "] Creating configuration file.");
+			try{
+				new File(getDataFolder(), "config.yml").createNewFile();
+				getConfig().set("msg1", "You renamed:");
+				getConfig().set("msg2", "to");
+				getConfig().set("msg3", "Watch your inventory!");
+				try{
+					getConfig().save(new File(getDataFolder(), "config.yml"));
+					load();
+				}catch (IOException e2){
+					e2.printStackTrace();
+				}
+			}catch(Exception e1){
+				e1.printStackTrace();
+			}
+    	}catch (IOException e){
+			log.warning("[" + getDescription().getName() + "] Cannot load configuration: " + e);
+		}catch (InvalidConfigurationException e){
+			log.warning("[" + getDescription().getName() + "] Cannot read configuration: " + e);
+		}
+    }
     
 	@Override
 	public void onEnable() {
 		log.info("[" + getDescription().getName() + "] now enabling!");
-	    getCommand("renamee").setExecutor(new Comando());
-	    getCommand("rename").setExecutor(new Comando());
+	    getCommand("renamee").setExecutor(new Comando(this));
+	    getCommand("rename").setExecutor(new Comando(this));
+	    getCommand("renamehand").setExecutor(new Comando(this));
+	    getCommand("renameehand").setExecutor(new Comando(this));
+	    getCommand("itemrename").setExecutor(new Comando(this));
+		load();
 	    try {
 	        BukkitMetrics metrics = new BukkitMetrics(this);
 	        metrics.start();
